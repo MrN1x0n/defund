3d7036d4d4ac3506263f3bccf05f0963bef2008d
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-05-17 15:34:21 +0300

    Update defund.sh

--- defund.sh
+++ defund.sh
@@ -12,7 +12,7 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://github.com/MrN1x0n/MrN1x0n/raw/main/logo.sh | bash && sleep 1
+sleep 1 && curl -s https://raw.githubusercontent.com/MrN1x0n/MrN1x0n/main/logo.sh | bash && sleep 1
 
 if [ ! $DEFUND_NODENAME ]; then
 read -p "Enter node name: " DEFUND_NODENAME
